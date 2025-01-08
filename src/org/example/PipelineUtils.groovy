b6ad72ac7c37fb3f48f1444a5f8f8ad0a53a0cff
Huy Pham <huy.pham@hudson.com>
2025-01-08 08:22:38 +0700

    update lib

--- src/org/example/PipelineUtils.groovy
+++ src/org/example/PipelineUtils.groovy
@@ -4,10 +4,10 @@ package org.example
 /* groovylint-disable-next-line ClassJavadoc, ClassName, ClassNameSameAsFilename, CompileStatic */
 class PipelineUtils {
 
-    void callDownstreamPipeline(String repoName) {
-        // echo 'PipelineUtils.groovy'
-        // echo "Triggering downstream pipeline for repository: ${repoName}"
-        build job: repoName, wait: true
+    static void callDownstreamPipeline(script, String repoName) {
+        script.echo 'PipelineUtils.groovy'
+        script.echo "Triggering downstream pipeline for repository: ${repoName}"
+        script.build(job: repoName, wait: true)
     }
 
 }
--- vars/callDownstreamPipeline.groovy
+++ vars/callDownstreamPipeline.groovy
@@ -2,8 +2,9 @@
 import org.example.PipelineUtils
 
 /* groovylint-disable-next-line MethodReturnTypeRequired, NoDef */
-def call(String repoName) {
+def call(script, String repoName) {
     // sh("echo 'callDownstreamPipeline.groovy'")
-    def myUtil = new PipelineUtils()
-    myUtil.callDownstreamPipeline(repoName)
+    // def myUtil = new PipelineUtils()
+    echo 'callDownstreamPipeline.groovy'
+    PipelineUtils.callDownstreamPipeline(script, repoName)
 }
